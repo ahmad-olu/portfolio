261df7b1ba3ef788202d06718a0da453bb1e2dde
ahmad olukotun <ahmadolukotun@gmail.com>
2024-12-09 05:05:40 +0100

    changed background,started the connect page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,39 +4,41 @@ export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+      ? "border-orange-600 text-orange-700"
+      : "border-transparent hover:border-orange-600 text-orange-300";
   return (
-    <nav class="bg-sky-800">
-      <div class="container mx-auto flex items-center justify-between p-3 text-gray-200">
-        {/* Left section: Image and Name */}
-        <div class="flex items-center space-x-3">
-          {/* Image */}
-          <img
-            src="/public/img/IMG.jpg"
-            alt="Ahmad olukotun"
-            class="w-10 h-10 rounded-md border border-gray-300"
-          />
-          {/* Name */}
-          <span class="text-lg font-bold">AHMAD OLU</span>
-        </div>
+    <div class="px-12 py-3">
+      <nav class=" border border-b-orange-600 rounded-xl">
+        <div class="container mx-auto flex items-center justify-between p-3 text-gray-200">
+          {/* Left section: Image and Name */}
+          <div class="flex items-center space-x-3">
+            {/* Image */}
+            <img
+              src="/public/img/IMG.jpg"
+              alt="Ahmad olukotun"
+              class="w-10 h-10 rounded-md border border-gray-300"
+            />
+            {/* Name */}
+            <span class="text-lg font-bold text-orange-600">AHMAD OLU</span>
+          </div>
 
-        {/* Right section: Navigation links */}
-        <ul class="flex space-x-6">
-          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-            <a href="/">Home</a>
-          </li>
-          <li class={`border-b-2 ${active("/story")} mx-1.5 sm:mx-6`}>
-            <a href="/story">Story</a>
-          </li>
-          <li class={`border-b-2 ${active("/album")} mx-1.5 sm:mx-6`}>
-            <a href="/album">Album</a>
-          </li>
-          <li class={`border-b-2 ${active("/connect")} mx-1.5 sm:mx-6`}>
-            <a href="/connect">Connect</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          {/* Right section: Navigation links */}
+          <ul class="flex space-x-6">
+            <li class={`border-b-2  ${active("/")} mx-1.5 sm:mx-6`}>
+              <a href="/">Home</a>
+            </li>
+            <li class={`border-b-2 ${active("/story")} mx-1.5 sm:mx-6`}>
+              <a href="/story">Story</a>
+            </li>
+            <li class={`border-b-2 ${active("/album")} mx-1.5 sm:mx-6`}>
+              <a href="/album">Album</a>
+            </li>
+            <li class={`border-b-2 ${active("/connect")} mx-1.5 sm:mx-6`}>
+              <a href="/connect">Connect</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
--- src/routes/connect.tsx
+++ src/routes/connect.tsx
@@ -3,21 +3,21 @@ import Counter from "~/components/Counter";
 
 export default function Connect() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
+    <main class="text-center mx-auto text-gray-700 ">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Connect Page
+        Connect
       </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
+      <br />
+      <p>
+        <a href="mailto:example@example.com">Tap to Send me an email</a>
+      </p>
+      <p>
+        <a href="https://www.twitter.com/yourprofile" target="_blank">
+          Twitter
+        </a>
+        <a href="https://www.linkedin.com/in/yourprofile" target="_blank">
+          LinkedIn
+        </a>
       </p>
       <p class="my-4">
         <A href="/" class="text-sky-600 hover:underline">
